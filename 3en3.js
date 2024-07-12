1bfdf8ebfb513a9742a81c256c47cbb879cfc849
jooseeruu <arjona2908@gmail.com>
2024-07-12 23:26:39 +0200

    cumplir

---
+++ 3en3.js
@@ -0,0 +1,10 @@
+function impresion() {
+  for (let index = 1; index <= 10; index += 3) {
+    console.log(index);
+  }
+  for (let j = 1; j >= -10; j -= 3) {
+    console.log(j);
+  }
+}
+
+impresion();
