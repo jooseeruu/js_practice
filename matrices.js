9354f92a8ad08e471567ca94fb6b1eea85001b95
jooseeruu <arjona2908@gmail.com>
2024-07-15 00:09:44 +0200

    jugando con matrices

---
+++ matrices.js
@@ -0,0 +1,10 @@
+let matrix = [
+  [10, 20, 30],
+  [40, 50, 60],
+];
+
+for (let filas = 0; filas < matrix.length; filas++) {
+  for (let columnas = 0; columnas < matrix[filas].length; columnas++) {
+    console.log(matrix[filas][columnas]);
+  }
+}
