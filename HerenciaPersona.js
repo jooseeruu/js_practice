875d25ddc0b2ad68a976f703933cfce5ca137f8a
jooseeruu <arjona2908@gmail.com>
2024-08-26 00:34:29 +0200

    well

---
+++ HerenciaPersona.js
@@ -0,0 +1,41 @@
+class Persona {
+  static Contador_Personas = 0;
+  constructor(nombre, apellidos, edad) {
+    this._nombre = nombre;
+    this._apellidos = apellidos;
+    this._edad = edad;
+    this._id = ++Persona.Contador_Personas;
+  }
+  get id() {
+    return this._id;
+  }
+  get nombre() {
+    return this._nombre;
+  }
+  get apellidos() {
+    return this._apellidos;
+  }
+  get edad() {
+    return this._edad;
+  }
+
+  set nombre(nombre) {
+    this._nombre = nombre;
+  }
+
+  set apellidos(apellidos) {
+    this._apellidos = apellidos;
+  }
+
+  set edad(edad) {
+    this._edad = edad;
+  }
+
+  nombreCompleto() {
+    return (
+      this._id + " " + this._nombre + " " + this._apellidos + " " + this._edad
+    );
+  }
+}
+let persona = new Persona("Mariano", "Diaz", 18);
+console.log(persona.nombreCompleto());
