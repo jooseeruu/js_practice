c9a812be457b86a3fd8da9a441328e0d6d3ea0b3
jooseeruu <arjona2908@gmail.com>
2024-09-01 00:26:18 +0200

    class orden

--- sistemaVentas.js
+++ sistemaVentas.js
@@ -34,13 +34,49 @@ class Producto {
 
 class Orden {
   static contadorOrdenes = 0;
+
   static get MAX_PRODUCTOS() {
     return 5;
   }
+
   constructor() {
-    this._idOrder = ++Orden.contadorOrdenes;
+    this._idOrden = ++Orden.contadorOrdenes;
     this._productos = [];
-    this._contadorProductosAgregados = 0;
+    //this._contadorProductosAgregados = 0;
+  }
+
+  get idOrden() {
+    return this._idOrden;
+  }
+
+  agregarProducto(producto) {
+    if (this._productos.length < Orden.MAX_PRODUCTOS) {
+      this._productos.push(producto);
+      //this._productos[this._contadorProductosAgregados++] = producto;
+    } else {
+      console.log("No se pueden agregar más productos");
+    }
+  }
+
+  calcularTotal() {
+    let totalVenta = 0;
+    for (let producto of this._productos) {
+      totalVenta += producto.precio; //totalVenta = totalVenta + producto.precio
+    }
+    return totalVenta;
+  }
+
+  mostrarOrden() {
+    let productosOrden = "";
+    for (let producto of this._productos) {
+      productosOrden += producto.toString() + " ";
+    }
+
+    console.log(
+      `Orden: ${
+        this._idOrden
+      } Total: $${this.calcularTotal()}, Productos: ${productosOrden} `
+    );
   }
 }
 
