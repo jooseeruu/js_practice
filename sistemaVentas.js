c743439e1d88416cd2b0782e38e9142e47cf8dc1
jooseeruu <arjona2908@gmail.com>
2024-09-02 00:28:10 +0200

    prueba sv

--- sistemaVentas.js
+++ sistemaVentas.js
@@ -82,5 +82,18 @@ class Orden {
 
 let producto1 = new Producto("Pantalón", 200);
 let producto2 = new Producto("Camisa", 100);
-console.log(producto1.toString());
-console.log(producto2.toString());
+
+let orden1 = new Orden();
+orden1.agregarProducto(producto1);
+orden1.agregarProducto(producto2);
+orden1.mostrarOrden();
+
+let orden2 = new Orden();
+let producto3 = new Producto("Cinturon", 50);
+orden2.agregarProducto(producto3);
+orden2.agregarProducto(producto1);
+orden2.agregarProducto(producto2);
+orden2.agregarProducto(producto3);
+orden2.agregarProducto(producto1);
+orden2.agregarProducto(producto2);
+orden2.mostrarOrden();
